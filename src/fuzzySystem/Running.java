6e74f9f1cea3155472907eab24fae738045268a6
Jkasnese <guilhermelw.uefs@gmail.com>
2018-07-27 11:28:59 -0300

    Added simulation system

---
+++ src/fuzzySystem/Running.java
@@ -0,0 +1,20 @@
+package fuzzySystem;
+
+public class Running {
+
+	public main {
+		CannonSystem cannon = new CannonSystem(23, 52, 0);
+		Integer dx = new Integer(0);
+		Integer dy = new Integer(0);
+
+		int cannonAngle = 45;
+		int shotSpeed = 30;
+
+		do {
+			cannon.shot(cannonAngle, shotSpeed, dx, dy);
+			feed fuzz 
+
+		} while (dy.getValue() != 0);
+	}
+	
+}
